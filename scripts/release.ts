66b6b35327d2aea49db9df546229768e1e9935fc
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-04 21:46:42 -0500

    Add scripts to release plugin to npm

---
+++ scripts/release.ts
@@ -0,0 +1,25 @@
+import { execSync } from 'node:child_process'
+
+const args = process.argv
+
+let versionType = 'patch'
+
+if (args.includes('--major')) versionType = 'major'
+if (args.includes('--minor')) versionType = 'minor'
+
+try {
+	console.log('🧪 Running tests...')
+	execSync('npm run test -- run', { stdio: 'inherit' })
+
+	console.log('🏗️ Building project...')
+	execSync('npm run build', { stdio: 'inherit' })
+
+	console.log(`📦 Bumping version (${versionType})...`)
+	execSync(`npm version ${versionType}`, { stdio: 'inherit' })
+
+	console.log('🚀 Publishing...')
+	execSync('npm publish', { stdio: 'inherit' })
+} catch (err) {
+	console.error(`❌ Release failed\n${err}`)
+	process.exit(1)
+}
